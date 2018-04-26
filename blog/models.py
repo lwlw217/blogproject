c108f798725925e3d8995f582f17d3cedb145341
wangle <wangle@ccoop.cn>
2018-04-26 10:22:49 +0800

    第二次commit

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
-
-
+import markdown
+from django.utils.html import strip_tags
 
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -16,11 +17,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 @python_2_unicode_compatible
 class Post(models.Model):
@@ -32,12 +35,29 @@ class Post(models.Model):
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(
+                extensions=[
+                    'markdown.extensions.extra',
+                    'markdown.extensions.codehilite',
+                ]
+            )
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-create_time']
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -84,7 +84,6 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'django.db.backends.mysql',
         'NAME':'blog',
         'USER':'dba',
         'PASSWORD':'zhtx1.q',
